4aba771cd3f285b64d2ef16c748bb7e5000d0ac2
Agoziem <chiagoziendukwe90@gmail.com>
2023-12-20 14:17:28 -0800

    changes

--- Student_Portal/views.py
+++ Student_Portal/views.py
@@ -39,18 +39,18 @@ def Result_Portal_view(request):
 			student = Students_Pin_and_ID.objects.get(student_name=student_name,student_class=studentClass,student_id=student_id,student_pin=Pin)
 			if Student_Result_Data.objects.filter(Student_name=student,Term=resultTerm,AcademicSession=resultSession).exists():
 				Student_Result_details=Student_Result_Data.objects.filter(Student_name=student,Term=resultTerm,AcademicSession=resultSession).first()
-				if studentClass.Class_section == 'Secondary':
-					print('is Secondary')
-					Student_Results=Result.objects.filter(students_result_summary=Student_Result_details)
-					for result in Student_Results:
-						labels.append(result.Subject.subject_name)
-						data.append(result.Total)
-				else:
+				if studentClass.Class_section == 'Primary':
 					print('is Primary')
 					Student_Results=PrimaryResult.objects.filter(students_result_summary=Student_Result_details)
 					for result in Student_Results:
 						labels.append(result.Subject.subject_name)
 						data.append(result.Total_100)
+				else:
+					print('is Secondary')
+					Student_Results=Result.objects.filter(students_result_summary=Student_Result_details)
+					for result in Student_Results:
+						labels.append(result.Subject.subject_name)
+						data.append(result.Total)
 				
 				if AnnualStudent.objects.filter(Student_name=student).exists():
 					Annual_Result=True
